3bf65f38b02baa527d69fa281679c8e0d9b388c5
Jade McDaniels <jademcdaniels81@gmail.com>
2021-02-22 09:42:26 -0500

    commit tree 8f8cac8fedba37cbde147d766abc3c14e85159c3

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+    drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver(name){
+
+    drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+}
+
+function appendDriver(name){
+    
+   let allDrivers = [...drivers, name]
+   return allDrivers
+}
+
+function prependDriver(name){
+    let allDrivers = [name, ...drivers]
+    return allDrivers
+}
+function removeLastDriver(){
+    return drivers.slice(0,-1)
+}
+    
+function removeFirstDriver(){
+    return drivers.slice(1)
+}
